35f7f668efadd153b1dd7d75c22f190bf64b8a85
lootexplorers <lootexplorers@gmail.com>
2021-12-26 18:28:16 +0800

    Added Explorer and Owner entity

--- generated/schema.ts
+++ generated/schema.ts
@@ -11,31 +11,27 @@ import {
   BigDecimal
 } from "@graphprotocol/graph-ts";
 
-export class ExampleEntity extends Entity {
+export class Owner extends Entity {
   constructor(id: string) {
     super();
     this.set("id", Value.fromString(id));
-
-    this.set("count", Value.fromBigInt(BigInt.zero()));
-    this.set("owner", Value.fromBytes(Bytes.empty()));
-    this.set("approved", Value.fromBytes(Bytes.empty()));
   }
 
   save(): void {
     let id = this.get("id");
-    assert(id != null, "Cannot save ExampleEntity entity without an ID");
+    assert(id != null, "Cannot save Owner entity without an ID");
     if (id) {
       assert(
         id.kind == ValueKind.STRING,
-        "Cannot save ExampleEntity entity with non-string ID. " +
+        "Cannot save Owner entity with non-string ID. " +
           'Considering using .toHex() to convert the "id" to a string.'
       );
-      store.set("ExampleEntity", id.toString(), this);
+      store.set("Owner", id.toString(), this);
     }
   }
 
-  static load(id: string): ExampleEntity | null {
-    return changetype<ExampleEntity | null>(store.get("ExampleEntity", id));
+  static load(id: string): Owner | null {
+    return changetype<Owner | null>(store.get("Owner", id));
   }
 
   get id(): string {
@@ -47,30 +43,56 @@ export class ExampleEntity extends Entity {
     this.set("id", Value.fromString(value));
   }
 
-  get count(): BigInt {
-    let value = this.get("count");
-    return value!.toBigInt();
+  get explorers(): Array<string> {
+    let value = this.get("explorers");
+    return value!.toStringArray();
   }
 
-  set count(value: BigInt) {
-    this.set("count", Value.fromBigInt(value));
+  set explorers(value: Array<string>) {
+    this.set("explorers", Value.fromStringArray(value));
+  }
+}
+
+export class Explorer extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+
+    this.set("owner", Value.fromString(""));
   }
 
-  get owner(): Bytes {
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save Explorer entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        "Cannot save Explorer entity with non-string ID. " +
+          'Considering using .toHex() to convert the "id" to a string.'
+      );
+      store.set("Explorer", id.toString(), this);
+    }
+  }
+
+  static load(id: string): Explorer | null {
+    return changetype<Explorer | null>(store.get("Explorer", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get owner(): string {
     let value = this.get("owner");
-    return value!.toBytes();
+    return value!.toString();
   }
 
-  set owner(value: Bytes) {
-    this.set("owner", Value.fromBytes(value));
-  }
-
-  get approved(): Bytes {
-    let value = this.get("approved");
-    return value!.toBytes();
-  }
-
-  set approved(value: Bytes) {
-    this.set("approved", Value.fromBytes(value));
+  set owner(value: string) {
+    this.set("owner", Value.fromString(value));
   }
 }
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { store } from "@graphprotocol/graph-ts"
 import {
   LootExplorers,
   Approval,
@@ -6,37 +6,36 @@ import {
   OwnershipTransferred,
   Transfer
 } from "../generated/LootExplorers/LootExplorers"
-import { ExampleEntity } from "../generated/schema"
+import { Explorer, Owner } from "../generated/schema"
+
+const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
+
+// Known burn addresses
+const BURN_ADDRESSES = [ZERO_ADDRESS]
 
 export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  // // Entities can be loaded from the store using a string ID; this ID
+  // // needs to be unique across all entities of the same type
+  // let entity = ExampleEntity.load(event.transaction.from.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  // // Entities only exist after they have been saved to the store;
+  // // `null` checks allow to create entities on demand
+  // if (!entity) {
+  //   entity = new ExampleEntity(event.transaction.from.toHex())
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+  //   // Entity fields can be set using simple assignments
+  //   entity.count = BigInt.fromI32(0)
+  // }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  // // BigInt and BigDecimal math are supported
+  // entity.count = entity.count + BigInt.fromI32(1)
 
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
+  // // Entity fields can be set based on event parameters
+  // entity.owner = event.params.owner
+  // entity.approved = event.params.approved
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
+  // // Entities can be written to the store with `.save()`
+  // entity.save()
 
   // It is also possible to access smart contracts from mappings. For
   // example, the contract that has emitted the event can be connected to
@@ -67,8 +66,32 @@ export function handleApproval(event: Approval): void {
   // - contract.totalSupply(...)
 }
 
-export function handleApprovalForAll(event: ApprovalForAll): void {}
+export function handleApprovalForAll(event: ApprovalForAll): void { }
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+export function handleOwnershipTransferred(event: OwnershipTransferred): void { }
 
-export function handleTransfer(event: Transfer): void {}
+export function handleTransfer(event: Transfer): void {
+  let id = event.params.tokenId.toString()
+  let contractId = event.address.toHex()
+  let from = event.params.from.toHex()
+  let to = event.params.to.toHex()
+  const contract = LootExplorers.bind(event.address)
+
+  if (BURN_ADDRESSES.includes(to)) {
+    // If sending to burn address, remove explorer from store
+    store.remove("Explorer", id)
+  } else {
+    // Set new owner and explorer
+    // Note: If a handler doesn't require existing field values, it is faster
+    // _not_ to load the entity from the store. Instead, create it fresh with
+    // `new Entity(...)`, set the fields that should be updated and save the
+    // entity back to the store. Fields that were not set or unset remain
+    // unchanged, allowing for partial updates to be applied.
+    let newOwner = new Owner(to)
+    let explorer = new Explorer(id)
+
+    explorer.owner = newOwner.id
+    explorer.save()
+    newOwner.save()
+  }
+}
